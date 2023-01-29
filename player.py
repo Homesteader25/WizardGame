ce0efe5feda0a8a1db6065c463993fdf3e2dfb92
Homesteader25 <jd083@uark.edu>
2023-01-29 11:15:35 -0600

    Player animations valid and now player can only jump when on the ground. Committed at 1:34:43 On the video.

--- player.py
+++ player.py
@@ -7,38 +7,76 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
         self.import_character_assets()
-        #STOPPED HERE AT 1:10:53 https://www.youtube.com/watch?v=YWN8GcmJ-jA#
         self.frame_index = 0
-        self.image = pygame.Surface((32, 50))
-        self.image.fill("red")
+        self.animation_speed = 0.15
+        self.image = self.animations['idle'][self.frame_index]
         self.rect = self.image.get_rect(topleft=pos)
 
         # player movement
         self.direction = pygame.math.Vector2(0, 0)
         self.speed = 6
         self.gravity = 0.8
-        self.jump_speed = -16
+        self.jump_speed = -14
+
+        # player status
+        self.status = 'idle'
+        self.facing_right = True
+        self.on_ground = False
+        self.on_ceiling = False
+        self.on_left = False
+        self.on_right = False
 
     def import_character_assets(self):
-        character_path = 'images/character'
+        character_path = 'images/character/'
         self.animations = {'idle': [], 'run': [], 'jump': [], 'attack': []}
 
         for animation in self.animations.keys():
             full_path = character_path + animation
+            print(full_path)
             self.animations[animation] = import_folder(full_path)
+            print(self.animations)
+
+    def animate(self):
+
+        animation = self.animations[self.status]
+
+        # loop over frame index
+        self.frame_index += self.animation_speed
+        if self.frame_index >= len(animation):
+            self.frame_index = 0
+
+        image = animation[int(self.frame_index)]
+        if self.facing_right:
+            self.image = image
+        else:
+            flipped_image = pygame.transform.flip(image, True, False)
+            self.image = flipped_image
 
     def get_input(self):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_RIGHT]:
             self.direction.x = 1
+            self.facing_right = True
         elif keys[pygame.K_LEFT]:
             self.direction.x = -1
+            self.facing_right = False
         else:
             self.direction.x = 0
 
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_SPACE] and self.on_ground:
             self.jump()
+
+    def get_status(self):
+        if self.direction.y < 0:
+            self.status = 'jump'
+        elif self.direction.y > 1:
+            self.status = 'jump'
+        else:
+            if self.direction.x != 0:
+                self.status = 'run'
+            else:
+                self.status = 'idle'
 
     def apply_gravity(self):
         self.direction.y += self.gravity
@@ -49,3 +87,5 @@ class Player(pygame.sprite.Sprite):
 
     def update(self):
         self.get_input()
+        self.get_status()
+        self.animate()
