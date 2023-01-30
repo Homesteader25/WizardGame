e1ad0329971f8157bddc9504c19a8c5c0f27a3a8
Homesteader25 <jd083@uark.edu>
2023-01-29 19:31:18 -0600

    Projectile placement fixed.

--- projectile.py
+++ projectile.py
@@ -5,17 +5,19 @@ class Projectile(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y, facing_right):
         pygame.sprite.Sprite.__init__(self)
         image = pygame.image.load("images/projectile.png")
-        pos_y -= 7
-        pos_x += 25
 
         if facing_right == True:
             self.image = image
+            pos_y -= 7
+            pos_x += 25
             self.rect = self.image.get_rect(center=(pos_x, pos_y))
-            self.direction = 5
+            self.direction = 10
         else:
+            pos_y -= 7
+            pos_x -= 25
             self.image = pygame.transform.flip(image, True, False)
             self.rect = self.image.get_rect(center=(pos_x, pos_y))
-            self.direction = -5
+            self.direction = -10
 
     def update(self, shift):
         self.rect.x += shift
